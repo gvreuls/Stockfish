3ea75f74b39fc0eb676e7c5db7d321a8673695f9
Guy Vreuls <guyvreuls@gmail.com>
2019-03-01 11:52:18 +0100

    Reduce contempt by nearness to the 50 moves draw
    Max. reduction 4/13
    
    Bench 3175644

--- src/search.cpp
+++ src/search.cpp
@@ -335,7 +335,7 @@ void Thread::search() {
   int ct = int(Options["Contempt"]) * PawnValueEg / 100; // From centipawns
 
   // Reduce contempt by nearness to the 50 moves draw.
-  ct = (300 - rootPos.rule50_count()) * ct / 300;
+  ct = (325 - rootPos.rule50_count()) * ct / 325;
 
   // In analysis mode, adjust contempt in accordance with user preference
   if (Limits.infinite || Options["UCI_AnalyseMode"])
