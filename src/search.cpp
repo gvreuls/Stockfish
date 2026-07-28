5ecb46f981a9fe4b32384a707f9c4ba981ce87c5
Guy Vreuls <guyvreuls@gmail.com>
2026-07-28 11:03:23 +0200

    Based on snicolet's formula.Take 3

--- src/search.cpp
+++ src/search.cpp
@@ -979,7 +979,7 @@ Value Search::Worker::search(
     // Step 8. Futility pruning: child node
     // The depth condition is important for mate finding.
     if (!ss->ttPv && eval >= beta && (!ttData.move || ttCapture) && !is_loss(beta)
-        && !is_win(eval) && depth < (9 + 10 / (1 + (std::abs(beta) + std::abs(eval)) / 3000)))
+        && !is_win(eval) && depth < (9 + 10 / (1 + (std::abs(beta) + std::abs(eval)) / 2000)))
     {
         Value futilityMult = std::min(45 + depth * 4, 85);
         futilityMult -= 20 * !ss->ttHit;
