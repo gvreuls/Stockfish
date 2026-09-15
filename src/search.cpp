eef329d962e288587b2f4d329b0ad0e47a917072
Guy Vreuls <guyvreuls@gmail.com>
2026-09-15 12:51:41 +0200

    Futility prune based on beta. Bench: 1564063

--- src/search.cpp
+++ src/search.cpp
@@ -1002,9 +1002,9 @@ Value Search::Worker::search(
     // Step 9. Futility pruning: child node
     // The depth condition is important for mate finding. It should NOT be tuned.
     if (!ss->ttPv && eval >= beta && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval)
-        && depth < (seekMate                ? 6
-                    : std::abs(eval) < 1100 ? 20
-                                            : 19))
+        && depth < (seekMate               ? 6
+                    : std::abs(beta) < 200 ? 20
+                                           : 19))
     {
         Value futilityMult = std::min(45 + depth * 4, 85);
         futilityMult -= 20 * !ss->ttHit;
